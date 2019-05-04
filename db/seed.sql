890a353a356c985a25a346a7ee39e27b0887c225
Kathy Chen <kathychen.social@gmail.com>
2019-05-04 00:23:21 -0400

    upload initial files

---
+++ db/seed.sql
@@ -0,0 +1,8 @@
+-- INSERT INTO burgers (name) VALUES ('Cheese burger');
+-- INSERT INTO burgers (name) VALUES ('Double cheese burger');
+-- INSERT INTO burgers (name) VALUES ('Spicy chicken burger');
+-- INSERT INTO burgers (name) VALUES ('Veggie burger');
+-- INSERT INTO burgers (name) VALUES ('Fish burger');
+-- INSERT INTO burgers (name) VALUES ('Steak sandwich');
+
+SELECT * FROM burgers;
